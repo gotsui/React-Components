4ce5a80912c4fb97764f18b12d20749985d8ae8b
gotsui <krhamui.2016@gmail.com>
2025-10-08 20:00:40 +0900

    テーブルのborderを修正、横幅いっぱいに広げるように変更

--- projects/node/src/app/table/_features/components/Table.tsx
+++ projects/node/src/app/table/_features/components/Table.tsx
@@ -16,28 +16,34 @@ const Table = ({
     const identifiedRowData: [string, Row][] = rowData.map((row) => [crypto.randomUUID(), row]);
 
     return (
-        <figure className="flex flex-col h-full">
+        <figure className="flex flex-col h-full w-full">
             {caption && (
-                <figcaption className="p-5 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
+                <figcaption className="pl-1 pb-1 text-lg font-semibold text-left rtl:text-right text-gray-900 bg-white dark:text-white dark:bg-gray-800">
                     {caption}
                 </figcaption>
             )}
-            <div className="flex-1 whitespace-nowrap overflow-auto border-t border-l">
-                <table className="table-auto border-separate border-spacing-0">
+            <div className="flex-1 whitespace-nowrap overflow-auto">
+                <table className="table-auto border-separate border-spacing-0 border-b border-l w-full">
                     <thead className="sticky top-0 z-10">
                         <tr className="bg-gray-200">
                             {columnDefs.map((def) => (
-                                <th key={def.field} className="px-4 py-2 border-r border-b">
+                                <th key={def.field} className="px-4 py-2 border-t border-r border-b text-xs">
                                     {def.lable ?? def.field}
                                 </th>
                             ))}
                         </tr>
                     </thead>
                     <tbody>
-                        {identifiedRowData.map(([rowId, row]) => (
+                        {identifiedRowData.map(([rowId, row], rowIndex) => (
                             <tr key={rowId}  className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 border-gray-200">
                                 {columnDefs.map((def) => (
-                                    <td key={def.field} className="px-4 py-2 border-r border-b">
+                                    <td
+                                        key={def.field}
+                                        className={[
+                                            "px-4 py-2 border-r text-xs",
+                                            `${rowIndex > 0 && "border-t"}`,
+                                        ].join(" ")}
+                                    >
                                         {row[def.field]}
                                     </td>
                                 ))}
